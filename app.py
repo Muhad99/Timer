661246cb86abff1f8bebecc22e4a5c70c57da659
NobleJoe123 <145128278+NobleJoe123@users.noreply.github.com>
2024-01-16 05:58:22 +0100

    add

--- app.py
+++ app.py
@@ -1,25 +1,82 @@
-from flask import Flask, flash, abort, render_template, Response, request
+from flask import Flask,render_template, Response, request, redirect, url_for, session
 from datetime import datetime
 from flask_bootstrap import Bootstrap
-import time
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+
 
 
 app = Flask(__name__)
 
+app.secret_key = 'key'
+
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'test'
+
+
+mysql = MySQL(app)
+
 bootstrap = Bootstrap(app)
 
 
+@app.route('/login/index')
+def home():
+    if 'loggedin' in session:
+        return render_template('index.html', username=session['username'])
+    return redirect(url_for('/log'))
 
-@app.route('/')
+
+
+
+@app.route('/index')
 def index():
     now = datetime.now()
     date_time = now.strftime("%H:%M:%S")
     return render_template('index.html', date_time=date_time)
-     
-           
-    
-    
 
+@app.route('/')
+def signup():
+    return render_template('register.html')
+     
+
+@app.route('/reg', methods=['GET', 'POST'])
+def reg():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor()
+        cursor.execute('INSERT INTO register VALUES (NULL, %s, %s, %s)', (username, password, email,))
+        mysql.connection.commit()
+        msg = 'You have successfully registered!'
+        return render_template('login.html', msg=msg)
+    elif request.method == 'POST':
+        msg = 'Please fill out the form!'
+    return render_template('register.html', msg=msg)
+    
+@app.route('/log', methods=['GET', 'POST'])
+def log():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM register WHERE username = % s AND password = % s', (username, password, ))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['Id'] = account['ID']
+            session['Username'] = account['Username']
+            msg = 'Logged in successfully !'
+            return render_template('base.html', msg = 'username')
+        else:
+            msg = 'Incorrect username / password !'
+    return render_template('login.html', msg = msg)
 
 if __name__ == '__main__':
  app.run(debug=True)
