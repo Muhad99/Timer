ef8373c909b237e467f562495016b16929ebc253
NobleJoe123 <145128278+NobleJoe123@users.noreply.github.com>
2024-01-21 16:45:56 +0100

    add

--- static/js/button.js
+++ static/js/button.js
@@ -16,8 +16,8 @@ const filterContainer = document.querySelector(".portfolio-filter"),
 
 for (let i = 0; i < totalFilterBtn; i++) {
     filterBtns[i].addEventListener("click", function () {
-        filterContainer.querySelector(".active").classList.remove("active");
-        this.classList.add("active");
+        filterContainer.querySelector(".active").classButton.remove("active");
+        this.classButton.add("active");
 
         const filterValue = this.getAttribute("data-filter");
         for (let k = 0; k < totalportfolioItem; k++) {
